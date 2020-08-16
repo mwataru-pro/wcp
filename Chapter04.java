8f96a9d7f89563781aa7909cdaddbb1f1d586c33
mwataru-pro <m1130wata@gmail.com>
2020-08-16 21:45:52 +0900

    Initial commit

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String str = "Wataru";
+    System.out.println(str);
+
+    str = "Mochidzuki";
+    System.out.println(str);
+
+    final int luckynumber = 7;
+    System.out.println(luckynumber);
+  }
+}
