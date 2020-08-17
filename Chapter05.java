420c5c1b0fff0074a8727b57a52bc3f88631c0bc
mwataru-pro <m1130wata@gmail.com>
2020-08-17 17:34:55 +0900

    Initial commit

---
+++ Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+  public static void main(String[] args) {
+    long long1 = 9223372036854775807L;
+    System.out.println(long1);
+
+    double double1 = 0.01d;
+    System.out.print(double1);
+
+    boolean boolean1 = false;
+    System.out.println(boolean1);
+
+    Float float1 = 0.1234f;
+    System.out.println(float1);
+
+    Integer integer1 = 1;
+    System.out.println(integer1);
+
+    Character character1 = 'A';
+    System.out.println(character1);
+
+    int[] array5 = new int[] { 10, 20, 30, 40, 50 };
+    System.out.println(array5[3]);
+  }
+}
